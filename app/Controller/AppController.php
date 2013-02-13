b9f25d65c0564a412c40ede10d4b9b7919930437
hotoracle <hotoracle2@gmail.com>
2013-02-13 19:20:51 +0100

    working on OpAuth, could not plugin folder not included

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -33,14 +33,13 @@ App::uses('Controller', 'Controller');
  */
 class AppController extends Controller {
 
-        public $components = array( 'Session', 'Error');
+        public $components = array('Auth','Session', 'Error');
 
         public function beforeFilter() {
-//                $this->Auth->authenticate = array('Form');
-//                
-//                $this->Auth->loginRedirect = array('action' => 'home', 'controller' => 'users');
-//                $this->Auth->logoutRedirect = array('action' => 'home', 'controller' => 'pages');
-//                $this->Auth->authError = 'You are not allowed to see that.';
+                $this->Auth->authenticate = array('Form');
+               $this->Auth->loginRedirect = array('action' => 'home', 'controller' => 'users');
+               $this->Auth->logoutRedirect = array('action' => 'home', 'controller' => 'pages');
+              $this->Auth->authError = 'You are not allowed to see that.';
 
                 # To enable portuguese language as main
                 #Configure::write('Config.language', 'por');
